8f8737fed44031d9e5f591f0abe02f49d82f3102
Pavel Brodsky <mcouthon@gmail.com>
2018-01-24 16:30:16 +0200

    The `use_profile` function is incorporated into the cluster module

--- plugins/cmom/cmom/cluster/utils.py
+++ plugins/cmom/cmom/cluster/utils.py
@@ -1,32 +1,15 @@
-from cloudify import ctx
-
+from ..common import workdir
 from ..common import execute_and_log as _execute_and_log
 
-DEFAULT_TENANT = 'default_tenant'
 
-
-def execute_and_log(cmd, no_log=False):
+def execute_and_log(cmd, deployment_id=None, no_log=False):
     return _execute_and_log(
         cmd,
         clean_env=True,
-        deployment_workdir=True,
+        deployment_workdir=workdir(deployment_id),
         no_log=no_log
     )
 
 
-def use_profile(config, cert):
-    manager = config['manager']
-    security = manager['security']
-
-    execute_and_log([
-        'cfy', 'profiles', 'use', manager['public_ip'],
-        '-u', security['admin_username'],
-        '-p', security['admin_password'],
-        '-t', DEFAULT_TENANT,
-        '-c', cert, '--ssl'
-    ])
-
-
-def use_master_profile():
-    master_profile = ctx.instance.runtime_properties['master']
-    execute_and_log(['cfy', 'profiles', 'use', master_profile])
+def use_profile(manager_ip):
+    execute_and_log(['cfy', 'profiles', 'use', manager_ip])
