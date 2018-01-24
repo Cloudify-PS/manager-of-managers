456d1fb2eb1ad906d4ee77c826c5a4e947e3215a
Pavel Brodsky <mcouthon@gmail.com>
2018-01-24 16:31:02 +0200

    Reworked the `start_cluster` operation and added a `clear_data` op

--- plugins/cmom/cmom/cluster/cluster.py
+++ plugins/cmom/cmom/cluster/cluster.py
@@ -1,45 +1,76 @@
 #!/usr/bin/env python
 
+import shutil
+
 from cloudify import ctx
 from cloudify.decorators import operation
 from cloudify.state import ctx_parameters as inputs
 
-from ..common import download_certificate
+from ..common import download_certificate, workdir
 
 from .utils import execute_and_log, use_profile
+from .maintenance import restore, backup, UpgradeConfig
 
 
-def _start_cluster(master_config, cert):
-    ctx.logger.info('Master {0} starting cluster'.format(master_config))
+def _start_cluster(master_config):
+    ctx.logger.info('Master starting cluster [{0}]'.format(master_config))
 
-    use_profile(master_config, cert)
+    use_profile(master_config['public_ip'])
 
     execute_and_log([
         'cfy', 'cluster', 'start',
-        '--cluster-host-ip', master_config['manager']['private_ip'],
-        '--cluster-node-name', master_config['manager']['public_ip']
+        '--cluster-host-ip', master_config['private_ip'],
+        '--cluster-node-name', master_config['public_ip']
     ])
 
 
-def _join_cluster(master_config, slave_config, cert):
-    ctx.logger.info('Slave {0} joining the cluster'.format(slave_config))
+def _join_cluster(master_config, slave_config):
+    ctx.logger.info('Slave joining the cluster [{0}]'.format(slave_config))
 
-    use_profile(slave_config, cert)
+    use_profile(slave_config['public_ip'])
 
     execute_and_log([
         'cfy', 'cluster', 'join',
-        '--cluster-host-ip', slave_config['manager']['private_ip'],
-        '--cluster-node-name', slave_config['manager']['public_ip'],
-        master_config['manager']['public_ip']
+        '--cluster-host-ip', slave_config['private_ip'],
+        '--cluster-node-name', slave_config['public_ip'],
+        master_config['public_ip']
     ])
 
 
-def _set_cluster_outputs(master, slaves):
-    """ Set up `master` and `slaves` runtime props to be used in outputs """
+def _set_cluster_outputs():
+    """
+    Set up the `managers` runtime prop to only contain the IPs of the managers,
+    as the rest of the info should already be present in the CLI profiles
 
-    ctx.instance.runtime_properties['master'] = master['manager']['public_ip']
-    slave_ips = [slave['manager']['public_ip'] for slave in slaves]
-    ctx.instance.runtime_properties['slaves'] = slave_ips
+    :return A list of dict with public + private IPs of managers
+    """
+    managers_config = ctx.instance.runtime_properties['managers']
+    managers = []
+    for manager in managers_config:
+        managers.append({
+            'public_ip': manager['manager']['public_ip'],
+            'private_ip': manager['manager']['private_ip'],
+        })
+    ctx.instance.runtime_properties['managers'] = managers
+    ctx.instance.update()
+
+    return managers
+
+
+def _create_cli_profiles():
+    managers = ctx.instance.runtime_properties['managers']
+    ca_cert = download_certificate(inputs['ca_cert'], deployment_workdir=True)
+    for config in managers:
+        public_ip = config['manager']['public_ip']
+        security = config['manager']['security']
+
+        execute_and_log([
+            'cfy', 'profiles', 'use', public_ip,
+            '-u', security['admin_username'],
+            '-p', security['admin_password'],
+            '-t', 'default_tenant',
+            '-c', ca_cert, '--ssl'
+        ])
 
 
 @operation
@@ -51,19 +82,23 @@ def start_cluster(**_):
     This runs in the `start` operation of the default lifecycle of the
     `cloudify_cluster_config` node
     """
-    managers = ctx.instance.runtime_properties['managers']
-    master, slaves = managers[0], managers[1:]
-    ca_cert = download_certificate(inputs['ca_cert'], deployment_workdir=True)
-    _start_cluster(master, ca_cert)
+    config = UpgradeConfig()
+    config.validate()
+
+    _create_cli_profiles()
+    manager_ips = _set_cluster_outputs()
+
+    master, slaves = manager_ips[0], manager_ips[1:]
+    _start_cluster(master)
+
+    if config.backup:
+        config.snapshot_path = backup(deployment_id=config.old_deployment_id)
+
+    if config.restore:
+        restore(config)
 
     for slave in slaves:
-        _join_cluster(master, slave, ca_cert)
-
-    _set_cluster_outputs(master, slaves)
-
-    # Clear the runtime properties as they may contain sensitive data
-    ctx.instance.runtime_properties.pop('managers')
-    ctx.instance.update()
+        _join_cluster(master, slave)
 
 
 @operation
@@ -87,3 +122,19 @@ def preconfigure(**_):
     # Clear the configuration from the manager's runtime properties
     ctx.target.instance.runtime_properties.pop('config')
     ctx.target.instance.update()
+
+
+@operation
+def clear_data(**_):
+    """
+    Remove the current deployment's workdir, as it remaining here can cause
+    potential problems down the road if a deployment with the same name
+    will be recreated
+    """
+
+    ctx.logger.info(
+        'Removing cluster data associated with deployment {0}'.format(
+            ctx.deployment.id
+        )
+    )
+    shutil.rmtree(workdir(), ignore_errors=True)
