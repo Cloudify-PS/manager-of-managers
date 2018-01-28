80fab8731120dc9d1e2b0481a0915174508559fe
Pavel Brodsky <mcouthon@gmail.com>
2018-01-28 18:03:22 +0200

    Allow ignoring errors

--- plugins/cmom/cmom/cluster/utils.py
+++ plugins/cmom/cmom/cluster/utils.py
@@ -2,12 +2,16 @@ from ..common import workdir
 from ..common import execute_and_log as _execute_and_log
 
 
-def execute_and_log(cmd, deployment_id=None, no_log=False):
+def execute_and_log(cmd,
+                    deployment_id=None,
+                    no_log=False,
+                    ignore_errors=False):
     return _execute_and_log(
         cmd,
         clean_env=True,
         deployment_workdir=workdir(deployment_id),
-        no_log=no_log
+        no_log=no_log,
+        ignore_errors=ignore_errors
     )
 
 
--- plugins/cmom/cmom/common.py
+++ plugins/cmom/cmom/common.py
@@ -10,7 +10,8 @@ CERTIFICATES = 'certificates'
 def execute_and_log(cmd,
                     clean_env=False,
                     deployment_workdir=None,
-                    no_log=False):
+                    no_log=False,
+                    ignore_errors=False):
     """
     Execute a command and log each line of its output as it is printed to
     stdout
@@ -24,6 +25,7 @@ def execute_and_log(cmd,
     :param deployment_workdir: If set to true instead of using the default
         .cloudify folder, we use a folder that depends on the deployment ID
     :param no_log: If set to True the output will not be logged
+    :param ignore_errors: Don't raise an exception on errors if True
     """
     env = os.environ.copy()
     if clean_env:
@@ -35,7 +37,7 @@ def execute_and_log(cmd,
     proc = _run_process(cmd, env)
     output = _process_output(proc, not no_log)
     return_code = _return_code(proc)
-    if return_code:
+    if return_code and not ignore_errors:
         raise CommandExecutionException(
             cmd, error=output, output=output, code=return_code
         )
