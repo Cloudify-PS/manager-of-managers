961736f9bf36cf60c1e5cb4225b071e5a25e3d2d
Pavel Brodsky <mcouthon@gmail.com>
2018-03-21 11:32:00 +0200

    Bump the plugin version

--- plugins/cmom/setup.py
+++ plugins/cmom/setup.py
@@ -2,13 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='cloudify-manager-of-managers',
-    version='1.1',
+    version='1.2',
     author='Cloudify',
     author_email='hello@cloudify.co',
     packages=find_packages(include='cmom*'),
     description='Cloudify Manager of Managers plugin',
     install_requires=[
-        # 'cloudify-plugins-common>=4.3'
-        'cloudify-plugins-common'
+        'cloudify-plugins-common==4.3'
     ],
 )
