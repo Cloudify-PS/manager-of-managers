e61dbf35d3f44040362373fdc195353494389ca3
Pavel Brodsky <mcouthon@gmail.com>
2018-04-16 13:28:59 +0300

    Create a new `profile` module to handle working with cfy profiles

---
+++ plugins/cmom/cmom/cluster/profile.py
@@ -0,0 +1,152 @@
+from uuid import uuid4
+from contextlib import contextmanager
+
+from cloudify import ctx
+from cloudify.exceptions import (
+    CommandExecutionException,
+    NonRecoverableError,
+    RecoverableError
+)
+
+from .utils import execute_and_log
+from ..common import DEFAULT_TENANT
+
+
+def get_current_master(instance=None):
+    instance = instance or ctx.instance
+    runtime_props = instance.runtime_properties
+    managers, ca_cert = get_config(runtime_props)
+
+    cluster_profile = _get_cluster_profile(managers, instance)
+    with profile(cluster_profile, instance):
+        new_master = _get_cluster_master()
+
+    _update_new_master(new_master, instance, managers)
+    return new_master
+
+
+@contextmanager
+def profile(manager_ip=None, instance=None):
+    manager_ip = manager_ip or get_current_master(instance)
+    instance = instance or ctx.instance
+    managers, _ = get_config(instance.runtime_properties)
+    temp_profile_name = None
+    try:
+        temp_profile_name = _create_profile(
+            manager_ip,
+            instance.runtime_properties
+        )
+        yield temp_profile_name
+    finally:
+        if temp_profile_name:
+            execute_and_log(
+                ['cfy', 'profiles', 'delete', temp_profile_name],
+                ignore_errors=True,
+                no_log=True
+            )
+
+
+def _update_new_master(new_master, instance, managers):
+    for manager, manager_config in managers.items():
+        is_master = manager == new_master
+        manager_config['is_master'] = is_master
+
+    # Only updating when needed, to avoid conflicts during node-instance update
+    if instance.runtime_properties.dirty:
+        instance.runtime_properties['managers'] = managers
+        instance.update()
+
+
+def _get_cluster_master():
+    """
+    Return the IP of the current cluster leader. This is relevant after a
+    failover, when the master has changed
+    """
+    output = execute_and_log(['cfy', 'cluster', 'nodes', 'list'], no_log=True)
+    for line in output.split('\n'):
+        if 'leader' in line:
+            leader_ip = line.split('|')[2].strip()
+            ctx.logger.info('The current leader is: {0}'.format(leader_ip))
+            return leader_ip
+
+    raise RecoverableError(
+        'Could not find a cluster leader in the cluster profile. This might '
+        'mean that the cluster is in an undefined state (e.g. midway of '
+        'HA failover)'
+    )
+
+
+def get_config(runtime_props):
+    """
+    Return a tuple with the `managers` config and CA cert path.
+    Raise an exception if either of those does not appear in the runtime props
+    :param runtime_props: The runtime properties dict from which to get the
+    values
+    """
+    managers = runtime_props.get('managers')
+    ca_cert = runtime_props.get('ca_cert')
+    missing_value = None
+    if not managers:
+        missing_value = 'managers'
+    elif not ca_cert:
+        missing_value = 'ca_cert'
+    if missing_value:
+        raise NonRecoverableError(
+            'Could not load `{0}` config from the runtime '
+            'properties. This probably means that the blueprint '
+            'was not installed correctly.'.format(missing_value))
+    return managers, ca_cert
+
+
+def _create_profile(manager_ip, runtime_props):
+    managers, ca_cert = get_config(runtime_props)
+    config = managers[manager_ip]
+    profile_name = str(uuid4())
+
+    execute_and_log([
+        'cfy', 'profiles', 'use', config['public_ip'],
+        '-u', config['admin_username'],
+        '-p', config['admin_password'],
+        '-t', DEFAULT_TENANT,
+        '-c', ca_cert,
+        '--ssl',
+        '--profile-name', profile_name
+    ], no_log=True)
+    # If working with clusters, make sure the profile is recognized as
+    # a cluster profile
+    execute_and_log(
+        ['cfy', 'cluster', 'update-profile'],
+        no_log=True,
+        ignore_errors=True
+    )
+    return profile_name
+
+
+def _get_cluster_profile(managers, instance=None):
+    """
+    Try to use all of the available profiles, until you find one which has
+    the cluster configured on, and return it
+    """
+    for manager_ip, manager_config in managers.items():
+        ctx.logger.info('Trying: {0}'.format(manager_ip))
+        try:
+            with profile(manager_ip, instance=instance):
+                execute_and_log(
+                    ['cfy', 'cluster', 'status'],
+                    no_log=True
+                )
+                execute_and_log(
+                    ['cfy', 'cluster', 'update-profile'],
+                    no_log=True
+                )
+                ctx.logger.info('Found cluster profile: '
+                                '{0}'.format(manager_ip))
+
+                return manager_ip
+        except CommandExecutionException:
+            pass
+
+    raise RecoverableError(
+        'Could not find a profile with a cluster configured. This '
+        'might mean that the whole network is unreachable.'
+    )
--- plugins/cmom/cmom/cluster/utils.py
+++ plugins/cmom/cmom/cluster/utils.py
@@ -1,15 +1,5 @@
-import os
-import json
-from uuid import uuid4
-from contextlib import contextmanager
-
-from cloudify import ctx
-
-from ..common import workdir, DEFAULT_TENANT
+from ..common import workdir
 from ..common import execute_and_log as _execute_and_log
-
-
-CLUSTER_CONFIG = '.cluster'
 
 
 def execute_and_log(cmd,
@@ -23,69 +13,3 @@ def execute_and_log(cmd,
         no_log=no_log,
         ignore_errors=ignore_errors
     )
-
-
-@contextmanager
-def profile(manager_ip, deployment_id=None):
-    deployment_id = deployment_id or ctx.deployment.id
-    temp_profile_name = None
-    try:
-        temp_profile_name = _create_profile(manager_ip, deployment_id)
-        yield temp_profile_name
-    finally:
-        if temp_profile_name:
-            execute_and_log(
-                ['cfy', 'profiles', 'delete', temp_profile_name],
-                ignore_errors=True,
-                no_log=True
-            )
-
-
-def _create_profile(manager_ip, deployment_id):
-    cluster_config = load_cluster_config(deployment_id)
-    config = cluster_config['managers'][manager_ip]
-    profile_name = str(uuid4())
-
-    execute_and_log([
-        'cfy', 'profiles', 'use', config['public_ip'],
-        '-u', config['admin_username'],
-        '-p', config['admin_password'],
-        '-t', DEFAULT_TENANT,
-        '-c', cluster_config['ca_cert'],
-        '--ssl',
-        '--profile-name', profile_name
-    ], deployment_id=deployment_id)
-    # If working with clusters, make sure the profile is recognized as
-    # a cluster profile
-    execute_and_log(
-        ['cfy', 'cluster', 'update-profile'],
-        no_log=True,
-        ignore_errors=True,
-        deployment_id=deployment_id
-    )
-    return profile_name
-
-
-def load_cluster_config(deployment_id=None):
-    deployment_id = deployment_id or ctx.deployment.id
-    config_path = os.path.join(workdir(deployment_id), CLUSTER_CONFIG)
-    if not os.path.isfile(config_path):
-        return {}
-
-    with open(config_path, 'r') as f:
-        return json.load(f)
-
-
-def dump_cluster_config(new_config, deployment_id=None):
-    deployment_id = deployment_id or ctx.deployment.id
-    config_path = os.path.join(workdir(deployment_id), CLUSTER_CONFIG)
-    with open(config_path, 'w') as f:
-        json.dump(new_config, f)
-
-
-def get_current_master(deployment_id=None):
-    deployment_id = deployment_id or ctx.deployment.id
-    managers = load_cluster_config(deployment_id)['managers']
-    for manager_ip, manager_config in managers.items():
-        if manager_config.get('is_master'):
-            return manager_ip
