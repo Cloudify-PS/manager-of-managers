447b16d3c4ef477de1dbca08d4e53d0e03a09bed
Pavel Brodsky <mcouthon@gmail.com>
2018-10-24 19:27:22 +0300

    Revert to using version 2.5.2 of the OpenStack plugin It seems that all subsequent versions don't handle fixed IPs properly

--- plugins/cmom/setup.py
+++ plugins/cmom/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cloudify-manager-of-managers',
-    version='1.5.3',
+    version='1.5.4',
     author='Cloudify',
     author_email='hello@cloudify.co',
     packages=find_packages(include='cmom*'),
