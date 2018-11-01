65314b63206606ca19b6576ecadd12cc814e9d68
Pavel Brodsky <mcouthon@gmail.com>
2018-11-01 16:13:03 +0200

    Bumped version to 2.0.1

--- plugins/cmom/setup.py
+++ plugins/cmom/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cloudify-manager-of-managers',
-    version='2.0.0',
+    version='2.0.1',
     author='Cloudify',
     author_email='hello@cloudify.co',
     packages=find_packages(include='cmom*'),
